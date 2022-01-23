a9e933f988f7289874aab675ad04c3e6979695d2
Anatoliy Kurinnyy <one.quaker@gmail.com>
2022-01-23 14:29:22 +0200

    parser update - filter/sort added, like parser fix

--- spider/youtube.py
+++ spider/youtube.py
@@ -5,6 +5,7 @@ import traceback
 import random
 import string
 from datetime import datetime
+from pprint import pprint
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -30,8 +31,8 @@ def get_driver():
 
     options = Options()
     options.binary_location = os.environ['FIREFOX_BIN']
-    if os.environ.get("HEADLESS") == "1":
-        options.add_argument("--headless")
+    if os.environ.get('HEADLESS') == '1':
+        options.add_argument('--headless')
 
     firefox_capabilities = webdriver.DesiredCapabilities.FIREFOX
     firefox_capabilities['marionette'] = True
@@ -73,53 +74,108 @@ def get_element(selector, driver, click=False):
 
 
 def parse_like(val):
-    valid_val = int(''.join([x for x in val if x.isdigit()]))
-    return valid_val
+    if not val:
+        return
+
+    if val.lower() == 'no likes':
+        return 0
+
+    try:
+        valid_val = int(''.join([x for x in val if x.isdigit()]))
+        return valid_val
+    except ValueError:
+        print(f'like parse error, wrong value: {val}')
 
 
 def parse_date(val):
     import re
-    regex = re.compile(r'^.*(\w{3})\s(\d{2}),\s(\d{4})$')
+    regex = re.compile(r'^.*(\w{3})\s(\d+),\s(\d{4})$')
     match = regex.match(val)
     if match:
         date_str = '-'.join([match.group(1), match.group(2), match.group(3)])
-        date_iso = datetime.strptime(date_str, '%b-%d-%Y').date().isoformat()
-        return date_iso
+        date_valid = datetime.strptime(date_str, '%b-%d-%Y').strftime('%d-%m-%Y')
+        return date_valid
 
 
-def parse(driver):
+def click_filter_button(driver):
+    filter_button_selector = 'ytd-toggle-button-renderer.ytd-search-sub-menu-renderer'
+    get_element(filter_button_selector, driver, click=True)  # click on filters button
+
+
+def apply_filter_sort(driver):
+    filter_selector = 'ytd-search-filter-group-renderer.ytd-search-sub-menu-renderer'
+    filter_item_selector = 'yt-formatted-string.ytd-search-filter-renderer'
+
+    click_filter_button(driver)
+    filter_type = driver.find_elements_by_css_selector(filter_selector)[-4] # filter type row
+    random_delay()
+    filter_type.find_elements_by_css_selector(filter_item_selector)[0].click() # type video
+    random_delay()
+
+    click_filter_button(driver)
+    filter_sort = driver.find_elements_by_css_selector(filter_selector)[-1] # filter sort row
+    random_delay()
+    filter_sort.find_elements_by_css_selector(filter_item_selector)[-2].click() # view count
+    random_delay()
+
+
+def parse(driver, scroll_count=3):
+    result = []
+
     driver.get('https://youtube.com')
 
     search_input = get_element('input#search', driver)
     search_input.send_keys(SEARCH_Q)
-    search_ok = get_element('button#search-icon-legacy', driver, click=True)
+    search_ok = get_element('button#search-icon-legacy', driver, click=True) # click on search button
 
+    apply_filter_sort(driver)
+
+    video_url_list = []
     video_selector = 'ytd-video-renderer .ytd-video-renderer a#video-title'
-    get_element(video_selector, driver)
 
-    video_url_list = [
-        dict(title=x.get_attribute('title'), url=x.get_attribute('href'))
-        for x in driver.find_elements_by_css_selector(video_selector)
-    ]
+    for i in range(1, scroll_count + 1): # scroll page
+        scroll_val = i * random.randint(1200, 1800)
+        print(f'scroll page to {scroll_val}')
+        driver.execute_script(f'window.scrollTo(0, {scroll_val})')
+        random_delay()
 
+        for x in driver.find_elements_by_css_selector(video_selector):
+            data = dict(title=x.get_attribute('title'), url=x.get_attribute('href'))
+            if data not in video_url_list:
+                video_url_list.append(data)
+
+    random.shuffle(video_url_list)
     for i in video_url_list:
         print(f'processing {i["url"]}')
         driver.get(i['url'])
 
         # skip_premium_button = get_element('div.ytd-mealbar-promo-renderer a.yt-simple-endpoint #button[aria-label="Skip trial"]', driver, click=True)
-        like_dislike_btn_row = [(x.text, x.get_attribute('aria-label')) for x in driver.find_elements_by_css_selector('div#info yt-formatted-string#text')]
-        stat = {'like': parse_like(x[1]) for x in like_dislike_btn_row if x[1] and 'like' in x[1]}
+        stat = {}
         stat.update(i)
 
+        like_dislike_btn_row = [(x.text, x.get_attribute('aria-label')) for x in driver.find_elements_by_css_selector('div#info yt-formatted-string#text')]
+        raw_like = [x[1] for x in like_dislike_btn_row if x[1] and 'like' in x[1]][0]
+        like = parse_like(raw_like)
+        stat.update(dict(like=like, raw_like=raw_like))
+
         info_row = driver.find_element_by_css_selector('div#info-text')
-        stat['view'] = int(''.join([x for x in info_row.find_element_by_css_selector('div#count').text if x in string.digits]))
+
+        raw_view = info_row.find_element_by_css_selector('div#count').text
+        stat['raw_view'] = raw_view
+        stat['view'] = int(''.join([x for x in raw_view if x in string.digits]))
+
         raw_pub_date = info_row.find_element_by_css_selector('div#info-strings yt-formatted-string').text
+        stat['raw_pub_date'] = raw_pub_date
         stat['pub_date'] = parse_date(raw_pub_date)
+
         print(stat)
+        result.append(stat)
+    return result
 
 
 try:
-    parse(driver)
+    data = parse(driver)
+    pprint(data)
 except Exception as e:
     print(e)
     traceback.print_exc()
